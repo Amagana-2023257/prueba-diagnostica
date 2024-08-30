ab3a8ea34be6df9daf0d76c23ed901fd60e9e1ae
Programmed in Kinal <programmedkinal@kinal.edu.gt>
2024-08-30 15:29:50 -0600

    add(model):add desarollador

---
+++ src/org/amagana/Model/Desarollador.java
@@ -0,0 +1,26 @@
+package org.amagana.Model;
+
+/**
+ *
+ * @author amagana
+ */
+public class Desarollador {
+    
+    private String lenguajeProgramacion;
+
+    public Desarollador() {
+    }
+
+    public Desarollador(String lenguajeProgramacion) {
+        this.lenguajeProgramacion = lenguajeProgramacion;
+    }
+
+    public String getLenguajeProgramacion() {
+        return lenguajeProgramacion;
+    }
+
+    public void setLenguajeProgramacion(String lenguajeProgramacion) {
+        this.lenguajeProgramacion = lenguajeProgramacion;
+    }
+    
+}
